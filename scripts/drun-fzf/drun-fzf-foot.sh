fe58e0fd3033caaeabf4b05926dd5191dcac1251
Heather7283 <heather7283@protonmail.com>
2024-04-19 14:19:36 +0400

    fixed drun script

--- scripts/drun-fzf/drun-fzf-foot.sh
+++ scripts/drun-fzf/drun-fzf-foot.sh
@@ -3,7 +3,8 @@
 IFS=$'\t' read -r name filename < <(~/.config/scripts/drun-fzf/parse-desktop-files.py | fzf \
   --no-clear \
   --with-nth 1 \
-  --nth 1)
+  --nth 1 \
+  --delimiter $'\t')
 
 if [ -z "$filename" ]; then
   exit 1
