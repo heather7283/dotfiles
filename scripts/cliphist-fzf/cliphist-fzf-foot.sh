ae48fb515c2b2d6727c1192b07eb6783f3a2acb4
Heather7283 <heather7283@protonmail.com>
2024-04-23 18:16:37 +0400

    [scripts] simplify cliphist script, remove piping to temp fifo

--- scripts/cliphist-fzf/cliphist-fzf-foot.sh
+++ scripts/cliphist-fzf/cliphist-fzf-foot.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
-# This script runs inside foot window, uses fzf to select clipboard item and sends it to pipe
 
-pipe="$1"
-
-if [ -z "$pipe" ]; then
-  echo "No pipe specified, exiting"
-  exit 1
-fi
-
-cliphist list | fzf \
+selected_entry="$(cliphist list | fzf \
   -i \
   --no-multi \
   --bind 'tab:up' \
@@ -27,5 +19,9 @@ cliphist list | fzf \
       --optimize 9 \
       --view-size ${FZF_PREVIEW_COLUMNS}x${FZF_PREVIEW_LINES}; \
     else echo {} | cliphist decode | sed -e "s/.\{${FZF_PREVIEW_COLUMNS}\}/&\n/g"; \
-  fi' > "$pipe"
+  fi')"
+
+if [ -n "$selected_entry" ]; then
+  echo -n "$selected_entry" | cliphist decode | wl-copy
+fi
 
--- scripts/cliphist-fzf/cliphist-fzf.sh
+++ scripts/cliphist-fzf/cliphist-fzf.sh
@@ -1,21 +1,4 @@
 #!/usr/bin/env bash
-# This script is meant to be called by keybind
 
-# Create named pipe udner /tmp
-pipe="/tmp/$(mktemp --dry-run cliphist_fzf_foot.XXXXXXXX)"
-mkfifo "$pipe"
-
-# Execute 2nd part of this script
-hyprctl dispatch exec \[float\;pin\;stayfocused\] -- foot --override colors.background=3D484D ~/.config/scripts/cliphist-fzf/cliphist-fzf-foot.sh "$pipe" >/dev/null
-
-# Read clipboard item from pipe
-read -r selected_item < "$pipe"
-
-# If selected item is not an empty string then copy it
-if [ -n "$selected_item" ]; then
-  echo -n "$selected_item" | cliphist decode | wl-copy
-fi
-
-# Cleanup
-rm "$pipe"
+hyprctl dispatch exec \[float\;pin\;stayfocused\] -- foot --override colors.background=3D484D ~/.config/scripts/cliphist-fzf/cliphist-fzf-foot.sh
 
