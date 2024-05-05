4530443bb45d203fb6466e97225298947c8a5403
Heather7283 <heather7283@protonmail.com>
2024-05-05 22:03:24 +0400

    [lf] fix previewer

--- lf/previewer.sh
+++ lf/previewer.sh
@@ -6,8 +6,9 @@ size_y=$3
 pos_x=$4
 pos_y=$5
 
-mime_description=$(file --brief --mime -- "$filename")
+no_cache=0
 
+mime_description=$(file --brief --mime -- "$filename")
 case "$mime_description" in
   image/*)
     chafa \
@@ -17,8 +18,7 @@ case "$mime_description" in
       --optimize 9 \
       --animate off \
       --size "${size_x}x${size_y}" \
-      "$filename" && success="yes"
-    exit 1
+      "$filename" && { success="yes"; no_cache=1; }
     ;;
   video/*)
     tmpfile="/tmp/$$_lfvideopreview.png"
@@ -30,21 +30,14 @@ case "$mime_description" in
       --optimize=9 \
       --animate=off \
       --size="${size_x}x${size_y}" \
-      "$tmpfile" && success="yes"
+      "$tmpfile" && { success="yes"; no_cache=1; }
     rm "$tmpfile"
-    exit 1
     ;;
   audio/*)
     mediainfo "$filename" && success="yes"
     ;;
-  application/x-tar*|application/zstd*|application/gzip*|application/x-xz*)
-    tar -tf "$filename" && success="yes"
-    ;;
-  application/zip*|application/java-archive*)
-    unzip -Z1 "$filename" && success="yes"
-    ;;
-  application/x-7z*)
-    7z l "$filename" | awk 'BEGIN { name_start = 999999999999 } /\s*Date\s*Time\s*Attr\s*Size\s*Compressed\s*Name\s*/ { for (i = 0; i <= length($0); i++) { if (substr($0, i, length($0)) == "Name") { name_start = i } } } { if (NR > name_start) { print substr($0, name_start, length($0)) } }'
+  application/x-tar*|application/zstd*|application/gzip*|application/x-xz*|application/zip*|application/java-archive*|application/x-7z*)
+    bsdtar -tf "$filename" && success="yes"
     ;;
   application/json*)
     jq . "$filename" --color-output && success="yes"
@@ -54,8 +47,8 @@ case "$mime_description" in
   #  ;;
 esac
 
-if echo "$mime_description" | grep -qP '(.*)charset=(?!binary)'; then
-  bat --theme=base16 "$filename" || cat "$filename" && success="yes"
+if [ -z "$success" ] && echo "$mime_description" | grep -qP '(.*)charset=(?!binary)'; then
+  bat "$filename" || cat "$filename" && success="yes"
 fi
 
 if [ -z "$success" ]; then
@@ -63,4 +56,6 @@ if [ -z "$success" ]; then
   echo ""
   echo "$mime_description"
 fi
+
+exit $no_cache
 
