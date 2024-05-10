a2738fe804258453fd4231cecc79950ce0a7a96f
Heather7283 <heather7283@protonmail.com>
2024-05-10 19:59:43 +0400

    [scripts] add firefox history browser script to repo

---
+++ scripts/firefox-history-fzf/firefox-history-fzf.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+hyprctl dispatch exec \[float\;pin\;stayfocused\] -- foot --override colors.background=3D484D -- ~/.config/scripts/firefox-history-fzf/firefox-history-fzf-foot.sh
+
