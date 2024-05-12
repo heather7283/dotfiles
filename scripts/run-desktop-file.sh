61bcafd22b7b5f70d69d2ec7efeae5ec96946b4f
Heather7283 <heather7283@protonmail.com>
2024-05-13 00:42:55 +0400

    [scripts] add search feature to run-desktop-file

--- scripts/run-desktop-file.sh
+++ scripts/run-desktop-file.sh
@@ -1,24 +1,34 @@
 #!/usr/bin/env bash
 
 if [ -z "$1" ]; then
+  echo "you need to specify full path to desktop file or its name"
   exit 1
 fi
 
 filename="$(realpath "$1")"
 if [ ! -f "$filename" ]; then
-  exit 1
+  # try to search for desktop file ourselves if provided path does not exist
+  filename="$(find ~/.local/share/applications/ /usr/share/applications/ -maxdepth 1 -type f -name "$1" -print -quit)"
+  if [ -z "$filename" ]; then
+    echo "$1 not found in ~/.local/share/applications/ /usr/share/applications/"
+    exit 1
+  fi
 fi
 
 is_term="$(grep -m1 -o -Pe '(?<=^Terminal\=).*' "${filename}")"
 exec_line="$(grep -m1 -o -Pe '(?<=^Exec\=).*' "${filename}" | sed 's/%[fFuUdDnNickvm]//g')"
 
 if [ -z "$exec_line" ]; then
+  echo "exec line not found in $filename"
   exit 1
 fi
 
+# we will append all extra args after exec_line
+shift 1
+
 if [ "$is_term" = "true" ]; then
-  hyprctl dispatch exec -- foot -- $exec_line >/dev/null
+  hyprctl dispatch exec -- foot -- $exec_line "$@" >/dev/null
 else
-  hyprctl dispatch exec -- $exec_line >/dev/null
+  hyprctl dispatch exec -- $exec_line "$@" >/dev/null
 fi
 
