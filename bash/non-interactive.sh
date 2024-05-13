927732ed169cba29faa0a860830c8dc1fd34098f
Heather7283 <heather7283@protonmail.com>
2024-05-13 15:23:36 +0400

    [scripts] make bash rc file less dumb

--- bash/non-interactive.sh
+++ bash/non-interactive.sh
@@ -1,1 +1,13 @@
+# remove $HOME/bin from PATH, we'll put it there ourselves later
+# (we need it to be the first entry)
+if [[ "$PATH" == *"$HOME/bin:"* ]]; then
+  PATH="${PATH/$HOME\/bin/}"
+fi
+# remove . from PATH (not the greatest idea to have it there)
+if [[ "$PATH" == *".:"* ]]; then
+  PATH="${PATH/.:/}"
+fi
+PATH="${PATH#:}"
+
 export PATH="$HOME/bin:$PATH"
+
