049914b51f05f02728027dc5b115bef6f08d0f10
heather7283 <heather7283@protonmail.com>
2024-06-06 19:46:26 +0400

    oops forgor about this file

--- scripts/foot-popup.sh
+++ scripts/foot-popup.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-# assuming cell_width=9 and cell_height=21, 80 cells wide and 24 cells tall
-hyprctl dispatch exec '[float;pin;stayfocused; size 720 504]' -- foot --override colors.background=3D484D -- "$@"
+hyprctl dispatch exec -- footclient --server-socket /tmp/foot-popup-server.sock --override colors.background=3D484D --app-id foot-popup -- "$@"
 
