8f27c6c64c881a51eef42f21a41f5519fc2cc6de
heather7283 <heather7283@protonmail.com>
2024-06-15 00:48:38 +0400

    nvim uwu

---
+++ nvim/lua/plugins/autopairs.lua
@@ -0,0 +1,21 @@
+local options = {
+  fast_wrap = {},
+  disable_filetype = { "TelescopePrompt", "vim" },
+  enable_check_bracket_line = false
+}
+
+require("nvim-autopairs").setup(options)
+
+-- insert () after selection functions in completion menu
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+local cmp = require("cmp")
+cmp.event:on(
+  "confirm_done",
+  cmp_autopairs.on_confirm_done()
+)
+
+local Rule = require('nvim-autopairs.rule')
+local autopairs = require("nvim-autopairs")
+
+autopairs.add_rule(Rule("$$", "$$", "tex"))
+
