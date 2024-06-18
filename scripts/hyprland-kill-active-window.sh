7977ba9709032ddd33a4ce36dcca4faeb93cb54c
heather7283 <heather7283@protonmail.com>
2024-06-18 15:51:54 +0400

    hyprland kill active window script

--- scripts/hyprland-kill-active-window.sh
+++ scripts/hyprland-kill-active-window.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
 addr="$(hyprctl activewindow -j | jq -r ".address")"
-pid="$(hyprctl clients -j | jq -r ".[] | select(.address == \""$addr"\").pid")"
+IFS=$'\t' read -r pid initial_class < <(hyprctl clients -j | jq -r ".[] | select(.address == \""$addr"\") | ( .pid, .initialClass )" | tr '\n' '\t')
 
-# if process only has 1 winows, close it and then force kill the process after 3 seconds
+# prevent killing foot server
+if [[ "$initial_class" = *foot* ]]; then
+  hyprctl dispatch closewindow address:"$addr"
+  exit
+fi
+
+# if process only has 1 window, close it and then force kill the process after 3 seconds
 if [ "$(hyprctl clients -j | jq -r "[.[] | select(.pid == $pid)] | length")" -le 1 ]; then
   hyprctl dispatch closewindow address:"$addr"
 
-  sleep 5
+  sleep 3
 
   # some windows might open confirmation popups before closing, check for this case
   if [ "$(hyprctl clients -j | jq -r "[.[] | select(.pid == $pid)] | length")" -le 1 ]; then
