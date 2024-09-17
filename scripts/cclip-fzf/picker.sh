9e9e57e437fa67d4ea0e7d45c3cbf0ca518eff57
heather7283 <heather7283@protonmail.com>
2024-09-17 18:22:13 +0400

    oopsie

--- scripts/cclip-fzf/picker.sh
+++ scripts/cclip-fzf/picker.sh
@@ -6,7 +6,7 @@ cclip list | fzf \
   --with-nth 3 \
   --delimiter "$(printf '\t')" \
   --scheme history \
-  --preview 'exec ~/.config/scripts/cclip-fzf/preview.sh {}' \
+  --preview 'exec ~/.config/scripts/cclip-fzf/previewer.sh {}' \
   --bind "ctrl-o:become(~/.config/scripts/cclip-fzf/opener.sh {1} {2} {3})" \
   --bind "enter:become(cclip get {1} | wl-copy -t {2})"
 
