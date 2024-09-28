df30a1718352ad16c53fe703340e93422df97164
heather7283 <heather7283@protonmail.com>
2024-09-28 18:05:20 +0400

    scripts: fix cclip-fzf

--- scripts/cclip-fzf/picker.sh
+++ scripts/cclip-fzf/picker.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
-cclip list | fzf \
+list_cmd="cclip list"
+export FZF_DEFAULT_COMMAND="$list_cmd"
+
+fzf \
   --ignore-case \
   --no-multi \
   --with-nth 3 \
   --delimiter "$(printf '\t')" \
   --scheme history \
+  --no-clear \
   --preview 'exec ~/.config/scripts/cclip-fzf/previewer.sh {}' \
   --bind "ctrl-o:become(~/.config/scripts/cclip-fzf/opener.sh {1} {2} {3})" \
-  --bind "enter:become(cclip get {1} | wl-copy -t {2})"
+  --bind "ctrl-x:execute-silent(cclip delete {1})+reload(${list_cmd})" \
+  --bind "ctrl-r:reload(${list_cmd})" \
+  --bind "enter:become(cclip get {1} | wl-copy -t {2}; sleep 0.05)"
+  # DO NOT REMOVE sleep 0.05 EVERYTHING BREAKS WITHOUT IT
 
