719cf80d0475df3c9d3de978c8230ce3f0a34276
heather7283 <heather7283@protonmail.com>
2024-09-29 22:37:23 +0400

    scripts: foot-popup takes exclusive lock

--- scripts/foot-popup.sh
+++ scripts/foot-popup.sh
@@ -1,4 +1,22 @@
 #!/bin/sh
 
-footclient --server-socket /tmp/foot-popup-server.sock --override colors.background=3D484D --app-id foot-popup -- "$@"
+if [ -z "$XDG_RUNTIME_DIR" ]; then
+    echo "XDG_RUNTIME_DIR is not set"
+    exit 1
+fi
+
+server_socket="${XDG_RUNTIME_DIR}/foot-popup-server.sock"
+if [ ! -S "$server_socket" ]; then
+    echo "$server_socket is not a socket"
+    exit 1
+fi
+
+# make sure there's only 1 popup at the same time
+lockfile="${XDG_RUNTIME_DIR}/foot-popup.lock"
+flock -n "$lockfile" \
+    footclient \
+    --server-socket "$server_socket" \
+    --override colors.background=3D484D \
+    --app-id foot-popup \
+    "$@"
 
