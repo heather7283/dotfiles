bb4783b1eb1aee807683deddfd622b5ef984825e
heather7283 <heather7283@protonmail.com>
2024-10-04 20:52:47 +0400

    scripts: use secure delete option for cclip

--- scripts/cclip-fzf/picker.sh
+++ scripts/cclip-fzf/picker.sh
@@ -12,7 +12,7 @@ fzf \
   --no-clear \
   --preview 'exec ~/.config/scripts/cclip-fzf/previewer.sh {}' \
   --bind "ctrl-o:become(~/.config/scripts/cclip-fzf/opener.sh {1} {2} {3})" \
-  --bind "ctrl-x:execute-silent(cclip delete {1})+reload(${list_cmd})" \
+  --bind "ctrl-x:execute-silent(cclip delete -s {1})+reload(${list_cmd})" \
   --bind "ctrl-r:reload(${list_cmd})" \
   --bind "enter:become(cclip get {1} | wl-copy -t {2}; sleep 0.05)"
   # DO NOT REMOVE sleep 0.05 EVERYTHING BREAKS WITHOUT IT
