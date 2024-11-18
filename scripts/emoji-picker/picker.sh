903ce1f7c49da321f9c398ae27f269cc9d32c27b
heather7283 <heather7283@protonmail.com>
2024-11-18 18:35:53 +0400

    scripts: remove --nth in emoji/unicode picker

--- scripts/emoji-picker/picker.sh
+++ scripts/emoji-picker/picker.sh
@@ -2,7 +2,6 @@
 
 selected="$(fzf <~/.config/scripts/emoji-picker/emoji-list.txt \
     -d "$(printf '\t')" \
-    --nth=2 \
     --bind 'ctrl-y:execute-silent(printf '%s' {1} | wl-copy -t text/plain\;charset=utf-8)')"
 [ -z "$selected" ] && exit
 
