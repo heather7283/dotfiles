53fff61663662553ffad5cfc49c8e3e8b130d365
heather7283 <heather7283@protonmail.com>
2024-11-18 15:38:09 +0400

    scripts: fix clipboard history previewer

--- scripts/cclip-fzf/previewer.sh
+++ scripts/cclip-fzf/previewer.sh
@@ -11,30 +11,38 @@ chafa_wrapper() {
         --view-size "${FZF_PREVIEW_COLUMNS}x${FZF_PREVIEW_LINES}"
 }
 
+success=0
+
 # images
 if [[ "$mime" =~ ^image/.*$ ]]; then
   cclip get "$id" | chafa_wrapper
+  success=1
 # hex colors
-elif [[ "$preview" =~ ^(#|0x)?[0-9a-fA-F]{6,8}$ ]]; then
-  item="${preview/#\#/}"
+elif [[ "$preview" =~ ^(#|0x)?[0-9a-fA-F]{6}([0-9a-fA-F]{2})?$ ]]; then
+  item="${preview#\#}"
+  item="${preview#0x}"
   r="$((16#${item:0:2}))"
   g="$((16#${item:2:2}))"
   b="$((16#${item:4:2}))"
   echo "#$item"
   echo "$r $g $b"
   printf "\033[48;2;${r};${g};${b}m %*s \033[0m" "$FZF_PREVIEW_COLUMNS" ""
+  success=1
 # youtube
-elif [[ "$preview" =~ ^https:\/\/www.youtube.com\/watch\?v= ]]; then
+elif [[ "$preview" =~ ^https:\/\/(www\.)?youtu\.?be(\.com\/watch\?v=)? ]]; then
   url="${preview#https://www.youtube.com/watch?v=}"
-  url="${url%&*}"
-  curl --no-progress-meter "https://img.youtube.com/vi/$url/3.jpg" | chafa_wrapper
-# youtube
-elif [[ "$preview" =~ ^https:\/\/youtu.be\/ ]]; then
-  url="${preview#https://youtu.be/}"
-  url="${url%\?*}"
-  curl --no-progress-meter "https://img.youtube.com/vi/$url/3.jpg" | chafa_wrapper
+  url="${url#https://youtu.be/}"
+  url="${url%%&*}"
+  url="${url%"${url##*[![:space:]]}"}" # strip trailing whitespace
+  if
+    curl --no-progress-meter "https://img.youtube.com/vi/${url}/3.jpg" | chafa_wrapper
+  then
+    success=1
+  fi
+fi
+
 # fallback
-else
+if [ "$success" = 0 ]; then
   cclip get "$id" | sed -e "s/.\{${FZF_PREVIEW_COLUMNS}\}/&\n/g"
 fi
 
