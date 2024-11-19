76b0b08fea72641c24ccae352d80c5cfd8bd2766
heather7283 <heather7283@protonmail.com>
2024-11-19 17:57:25 +0400

    scrpits: fix opener in clipboard history picker

--- scripts/cclip-fzf/picker.sh
+++ scripts/cclip-fzf/picker.sh
@@ -13,7 +13,7 @@ fzf \
   --preview 'exec ~/.config/scripts/cclip-fzf/previewer.sh {}' \
   --bind 'focus:transform-preview-label:date -d @{3} "+%a, %d %b %Y %H:%M:%S %Z"' \
   --preview-label-pos bottom \
-  --bind "ctrl-o:become(~/.config/scripts/cclip-fzf/opener.sh {1} {2} {3})" \
+  --bind "ctrl-o:become(~/.config/scripts/cclip-fzf/opener.sh {1} {2} {4})" \
   --bind "ctrl-x:execute-silent(cclip delete -s {1})+reload(${list_cmd})" \
   --bind "ctrl-r:reload(${list_cmd})" \
   --bind "enter:become(cclip get {1} | wl-copy -t {2}; sleep 0.05)"
