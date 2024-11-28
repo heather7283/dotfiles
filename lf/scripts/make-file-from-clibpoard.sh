364efc966e2b5b8976cd01af35f93e1532bc5c42
heather7283 <heather7283@protonmail.com>
2024-11-28 11:20:43 +0400

    lf: add make-file-from-clipboard

---
+++ lf/scripts/make-file-from-clibpoard.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+export _script_name="make-file-from-clibpoard"
+
+# shellcheck source=/home/heather/.config/lf/scripts/common-defs.sh
+source ~/.config/lf/scripts/common-defs.sh
+
+name="$(read_line "file name: ")"
+
+if [ ! $? = 0 ]; then
+  exit
+fi
+
+if [ -z "$name" ]; then
+  echo_info "empty file name"
+elif [ -e "$name" ]; then
+  die "$name already exists"
+else
+  wl-paste >"$name" && :reload && :select "$(realpath "$name")"
+fi
+
