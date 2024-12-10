2d8451a4e6e9a51a30ca0f8ae0e85304a79b1f02
heather7283 <heather7283@protonmail.com>
2024-12-10 22:37:12 +0400

    scripts: fix delete not working in cclip-fzf

--- scripts/cclip-fzf/picker.sh
+++ scripts/cclip-fzf/picker.sh
@@ -14,7 +14,7 @@ fzf \
   --bind 'focus:transform-preview-label:date -d @{3} "+%a, %d %b %Y %H:%M:%S %Z"' \
   --preview-label-pos bottom \
   --bind "ctrl-o:become(~/.config/scripts/cclip-fzf/opener.sh {1} {2} {4})" \
-  --bind "ctrl-x:execute-silent(cclip delete -s {1})+reload(${list_cmd})" \
+  --bind "ctrl-x:execute-silent(cclip -s delete {1})+reload(${list_cmd})" \
   --bind "ctrl-r:reload(${list_cmd})" \
   --bind "enter:become(cclip get {1} | wl-copy -t {2}; sleep 0.05)"
   # DO NOT REMOVE sleep 0.05 EVERYTHING BREAKS WITHOUT IT
