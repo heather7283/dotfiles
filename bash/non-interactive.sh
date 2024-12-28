cb0e09a6a9b59106c48f38d076f62e56907a0d49
heather7283 <heather7283@protonmail.com>
2024-12-28 21:19:22 +0400

    bash: less dumb rc file

--- bash/non-interactive.sh
+++ bash/non-interactive.sh
@@ -1,13 +1,11 @@
 # remove $HOME/bin from PATH, we'll put it there ourselves later
 # (we need it to be the first entry)
-if [[ "$PATH" == *"$HOME/bin:"* ]]; then
-  PATH="${PATH/$HOME\/bin/}"
+if [ -n "$HOME" ] && [[ "$PATH" == *"${HOME}/bin"* ]]; then
+    PATH="${PATH/$HOME\/bin/}"
+    PATH="${PATH//::/:}"
+    PATH="${PATH#:}"
+    PATH="${PATH%:}"
 fi
-# remove . from PATH (not the greatest idea to have it there)
-if [[ "$PATH" == *".:"* ]]; then
-  PATH="${PATH/.:/}"
-fi
-PATH="${PATH#:}"
 
-export PATH="$HOME/bin:$PATH"
+export PATH="${HOME}/bin:${PATH}"
 
