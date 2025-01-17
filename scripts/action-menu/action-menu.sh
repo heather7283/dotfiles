d66f1c842837e6eed4eb13d454c64c238eff332e
heather7283 <heather7283@protonmail.com>
2025-01-17 20:29:03 +0400

    scripts: make action-menu.sh case sensitive

--- scripts/action-menu/action-menu.sh
+++ scripts/action-menu/action-menu.sh
@@ -1,18 +1,37 @@
 #!/usr/bin/env bash
 
 level1=(
-  "Internet [S]earch"$'\t'"~/.config/scripts/firefox-web-search.sh"
+  "Internet [s]earch"$'\t'"~/.config/scripts/firefox-web-search.sh"
   "[7]tv browser"$'\t'"~/.config/scripts/7tv-browser/7tv-browser.sh"
-  "[T]ranslator"$'\t'"~/.config/scripts/deeplx-translator/deeplx-translator.sh"
-  "[E]moji picker"$'\t'"~/.config/scripts/emoji-picker/picker.sh"
-  "[U]nicode picker"$'\t'"~/.config/scripts/unicode-picker/picker.sh"
-  "[B]rowser history"$'\t'"~/.config/scripts/firefox-history-fzf/firefox-history-fzf.sh"
-  "Application [R]unner"$'\t'"~/.config/scripts/drun-fzf/drun-fzf.sh"
-  "[C]lipboard history"$'\t'"~/.config/scripts/cclip-fzf/picker.sh"
-  "[M]PRIS control"$'\t'"~/.config/scripts/mpris-control/player-control.sh"
-  "[A]udio control"$'\t'"pulsemixer"
-  "[Q]alculate"$'\t'"tmux new-session qalc"
+  "[t]ranslator"$'\t'"~/.config/scripts/deeplx-translator/deeplx-translator.sh"
+  "[e]moji picker"$'\t'"~/.config/scripts/emoji-picker/picker.sh"
+  "[u]nicode picker"$'\t'"~/.config/scripts/unicode-picker/picker.sh"
+  "[b]rowser history"$'\t'"~/.config/scripts/firefox-history-fzf/firefox-history-fzf.sh"
+  "Application [r]unner"$'\t'"~/.config/scripts/drun-fzf/drun-fzf.sh"
+  "[c]lipboard history"$'\t'"~/.config/scripts/cclip-fzf/picker.sh"
+  "[m]PRIS control"$'\t'"~/.config/scripts/mpris-control/player-control.sh"
+  "[a]udio control"$'\t'"pulsemixer"
+  "[q]alculate"$'\t'"tmux new-session qalc"
 )
+
+change_script="$(cat<<'EOF'
+query={q};
+
+if [ "${#query}" -gt 1 ]; then
+  query="${query: -1:1}";
+  printf "change-query(${query})";
+  plus_needed=1;
+fi;
+
+trigger="\[${query}\]";
+match="$(grep -m1 -noe "$trigger" {+f})";
+if [ -n "$match" ]; then
+  line="${match%%:*}";
+  if [ "$plus_needed" = 1 ]; then echo -n "+"; fi;
+  printf "deselect-all+pos(${line})+select+accept";
+fi
+EOF
+)"
 
 choose() {
   local -n arr="$1"
@@ -28,25 +47,10 @@ choose() {
     --sync \
     --with-shell 'bash -c' \
     --bind 'load:select-all' \
-    --bind 'change:transform:
-      query={q};
-
-      if [ "${#query}" -gt 1 ]; then
-        query="${query: -1:1}";
-        printf "change-query($query)";
-        plus_needed=1;
-      fi;
-
-      trigger="\[${query}\]";
-      match="$(grep -m1 -inoe "$trigger" {+f})";
-      if [ -n "$match" ]; then
-        n="${match%%:*}";
-        if [ "$plus_needed" = 1 ]; then echo -n "+"; fi;
-        printf "deselect-all+pos($n)+select+accept";
-      fi' \
+    --bind "change:transform:${change_script}" \
     --bind 'enter:deselect-all+select+accept' \
     --bind 'double-click:deselect-all+select+accept' \
-    --preview 'cat ~/.config/scripts/action-menu/art.txt; echo "${FZF_PREVIEW_COLUMNS}x${FZF_PREVIEW_LINES}"' \
+    --preview 'cat ~/.config/scripts/action-menu/art.txt' \
     --preview-window 'border-none' \
     --no-info \
     --no-scrollbar \
