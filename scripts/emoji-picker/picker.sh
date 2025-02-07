cf3949ba4a7e723453ea26d3a842b113e160fc66
heather7283 <heather7283@protonmail.com>
2025-02-07 08:20:29 +0400

    scripts: update emoji picker script

--- scripts/emoji-picker/picker.sh
+++ scripts/emoji-picker/picker.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-tab='	'
-IFS="$tab"
-
-selected="$(fzf -d "$tab" <~/.config/scripts/emoji-picker/emoji-list.txt)"
+selected="$(fzf <~/.config/scripts/emoji-picker/emoji-list.txt)"
 [ -z "$selected" ] && exit
 
 set -- $selected
 emoji="$1"
-description="$2"
 
-printf '%s' "$emoji" | wl-copy -t "text/plain;charset=utf-8"
+wl-copy -t "text/plain;charset=utf-8" "$emoji"
+
