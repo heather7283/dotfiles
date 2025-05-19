515dc74cd558d13381fa742f21459a67b24fe0ea
heather7283 <heather7283@protonmail.com>
2025-05-19 14:37:08 +0400

    scripts: minor style changes

--- lf/scripts/opener.sh
+++ lf/scripts/opener.sh
@@ -6,32 +6,32 @@ extension="${filename##*.}"
 # first match based on extension
 case "$extension" in
   png|jpeg|jpg|jxl|webp|gif)
-      exec mvi "$filename";;
+      exec mvi "$filename" ;;
   mp3|flac|opus|wav|ape)
-      exec mpv --no-video "$filename";;
+      exec mpv --no-video "$filename" ;;
   mkv|mp4)
-      exec mpv --force-window=immediate "$filename";;
+      exec mpv --force-window=immediate "$filename" ;;
   webm) # can be either video or music, so dont force window
-      exec mpv "$filename";;
+      exec mpv "$filename" ;;
   djvu|pdf)
-      exec zathura "$filename";;
-  docx)
-      exec libreoffice "$(realpath "$filename")";;
+      exec zathura "$filename" ;;
+  docx|odt)
+      exec libreoffice "$(realpath "$filename")" ;;
   html)
-      exec browser "$filename";;
+      exec browser "$filename" ;;
 esac
 
 # match based on mime type
 mime_description=$(file --brief --mime -- "$filename")
 case "$mime_description" in
   image/*)
-    exec mvi "$filename"
-    ;;
+    exec mvi "$filename" ;;
   video/*)
-    exec mpv --force-window=immediate "$filename"
-    ;;
+    exec mpv --force-window=immediate "$filename" ;;
   audio/*)
-    exec mpv --no-video "$filename"
+    exec mpv --no-video "$filename" ;;
+  application/vnd.openxmlformats-officedocument.wordprocessingml.document*)
+    exec libreoffice "$(realpath "$filename")" ;;
 esac
 
 if [[ "$mime_description" =~ charset= ]] && [[ ! "$mime_description" =~ charset=binary ]]; then
