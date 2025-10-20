0e059a5335f2852a55e2cbd329d6e2a0e94cb23b
heather7283 <heather7283@protonmail.com>
2025-10-20 21:49:07 +0400

    nvim: fix nvim-lspconfig

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -4,71 +4,59 @@ require("mason-lspconfig").setup()
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-local handlers = {
-  -- default handler
-  function(server_name)
-    require("lspconfig")[server_name].setup({
-      capabilities = capabilities
-    })
-  end,
+vim.lsp.config("*", {
+  capabilities = capabilities
+})
 
-  ["basedpyright"] = function()
-    -- man I heckin love lua
-    require("lspconfig").basedpyright.setup({
-      settings = {
-        basedpyright = {
-          analysis = { typeCheckingMode = "basic" }
-        }
+vim.lsp.config("lua_ls", {
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
       },
-      capabilities = capabilities,
-      root_dir = function(filename, bufnr) return vim.fn.matchstr(filename, ".*/") end
-    })
-  end,
-
-  ["lua_ls"] = function()
-    require("lspconfig").lua_ls.setup({
-      settings = {
-        Lua = {
-          diagnostics = {
-            globals = { "vim" },
-          },
-
-          runtime = {
-            version = 'LuaJIT'
-          },
-
-          workspace = {
-            checkThirdParty=false,
-            library = {
-              vim.fn.expand("$VIMRUNTIME/lua"),
-              vim.fn.expand("$VIMRUNTIME/lua/vim/lsp"),
-              vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy",
-            }
-          }
-        }
+      runtime = {
+        version = 'LuaJIT'
       },
-      capabilities = capabilities
-    })
-  end,
-}
-require("mason-lspconfig").setup_handlers(handlers)
+      workspace = {
+        checkThirdParty=false,
+        library = {
+          vim.fn.expand("$VIMRUNTIME/lua"),
+          vim.fn.expand("$VIMRUNTIME/lua/vim/lsp"),
+          vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy",
+        }
+      }
+    }
+  }
+})
+
+vim.lsp.config("basedpyright", {
+  settings = {
+    basedpyright = {
+      analysis = {
+        typeCheckingMode = "basic"
+      }
+    }
+  },
+  root_dir = function(filename, _) return vim.fn.matchstr(filename, ".*/") end
+})
 
 -- use system clangd
 local clangd_path = vim.fn.exepath("clangd");
 if clangd_path ~= "" then
-  require("lspconfig").clangd.setup({
+  vim.lsp.config("clangd", {
     cmd = {
       clangd_path,
       "--background-index",
       "--all-scopes-completion",
       "--completion-style=detailed",
       "--header-insertion=never",
-    },
-    capabilities = capabilities
+    }
   })
 end
 
-vim.keymap.set('n', 'K', vim.lsp.buf.hover)
+-- cooked af
+vim.cmd(":LspStart")
 
+vim.keymap.set('n', 'K', vim.lsp.buf.hover)
 vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename)
 
