8a4fcf554a7ac828e7cc82ffc6c9b78084a2461d
heather7283 <heather7283@protonmail.com>
2026-01-06 15:57:25 +0400

    scripts: add sway wrapper

---
+++ scripts/wrappers/sway.sh
@@ -0,0 +1,26 @@
+uid="$(id -u)"
+if [ -z "${uid}" ]; then
+    die "unable to determine current user's id"
+fi
+
+export XDG_RUNTIME_DIR="${TMPDIR:-/tmp}/${uid}-runtime-dir"
+export DBUS_SESSION_BUS_ADDRESS="unix:path=${XDG_RUNTIME_DIR}/dbus.sock"
+
+if [ ! -d "$XDG_RUNTIME_DIR" ]; then
+    mkdir -pv "$XDG_RUNTIME_DIR" || die "failed to create ${XDG_RUNTIME_DIR}"
+fi
+
+#gpu=/dev/dri/by-name/amdgpu-card
+#if [ ! -e "$gpu" ]; then
+#  echo "${gpu} doesn't exist!" >&2
+#  exit 1
+#fi
+#
+#real_gpu="$(realpath "$gpu")"
+#export WLR_DRM_DEVICES="$real_gpu"
+
+"$real_exe" --unsupported-gpu "$@"
+
+# cleanup
+exec s6-svscanctl -t "${XDG_RUNTIME_DIR}/s6/service"
+
