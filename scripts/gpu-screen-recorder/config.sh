b04e8d1012629d90450135f7a7dc7b4782aae599
heather7283 <heather7283@protonmail.com>
2026-01-06 19:35:50 +0400

    scripts: use h264 in gpu-screen-recorder

--- scripts/gpu-screen-recorder/config.sh
+++ scripts/gpu-screen-recorder/config.sh
@@ -1,3 +1,6 @@
 FPS=60
 REPLAY_BUFFER=60
 FORMAT=mkv
+
+# gsr warning: gsr_video_encoder_vaapi_start: black bars have been added to the video because of a bug in AMD drivers/hardware. Record with h264 codec instead (-k h264) to get around this issue
+CODEC=h264
