d92634612ad0c8ce3b41a069112ab189853aa2d4
heather7283 <heather7283@protonmail.com>
2026-05-13 17:43:39 +0400

    scripts: remove wallets_dir in monero-wallet-cli wrapper

--- scripts/wrappers/monero-wallet-cli.sh
+++ scripts/wrappers/monero-wallet-cli.sh
@@ -3,9 +3,8 @@
 config_dir="${XDG_CONFIG_HOME:?}/bitmonero"
 state_dir="${XDG_STATE_HOME:?}/bitmonero"
 data_dir="${XDG_DATA_HOME:?}/bitmonero"
-wallets_dir="${data_dir}/wallets"
 
-mkdir -p "$config_dir" "$state_dir" "$data_dir" "$wallets_dir" || die 'mkdir failed'
+mkdir -p "$config_dir" "$state_dir" "$data_dir" || die 'mkdir failed'
 
 set -- \
     --log-file "${state_dir}/monero-wallet-cli.log" \
