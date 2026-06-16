95cc7cd191b4c60358b54b2a3c395f611f486c1b
heather7283 <heather7283@protonmail.com>
2026-06-17 02:59:19 +0400

    tmpfiles: add systemd-tmpfiles definitions for dealing with symlinks

--- scripts/install-dots.sh
+++ scripts/install-dots.sh
@@ -1,35 +1,25 @@
 #!/bin/sh
 
-echo "This will overwrite following files and directories:"
-echo "~/.local/share/applications/"
-echo "~/bin"
-echo
-printf "All data in them will be lost! Continue? [y/N] "
-read -r answer
+ask() {
+    printf "$1 [y/N] "
+    read -r answer
+    case "$answer" in
+        (y|Y) return 0 ;;
+        (*) return 1 ;;
+    esac
+}
 
-if [ ! "$answer" = "y" ]; then
+echo "This will call systemd-tmpfiles, potentinally overwriting existing files!"
+if ! ask "Data loss may occur. Continue?"; then
   echo "Abort"
   exit 1
 fi
 
-cd ~
-
-if [ ! -d ~/.config/bin ]; then
-  echo "~/.config/bin doesn't exist, what have you done? clone the repo properly"
-  exit 1
-fi
+# termux doesn't have systemd-tmpfiles so do this manually
 rm -rfv ~/bin
 ln -sv .config/bin ~/bin
 
-if [ ! -d ~/.config/applications/ ]; then
-  echo "~/.config/applications/ not found, fix your repo bruh"
-  exit 1
-fi
-rm -rfv ~/.local/share/applications
-if [ ! -d ~/.local/share ]; then
-  mkdir -pv ~/.local/share/
-fi
-ln -sv ~/.config/applications ~/.local/share/applications
+systemd-tmpfiles --user --create
 
 echo "Done"
 echo "Make sure to put this in /etc/zsh/zshenv or whatever:"
